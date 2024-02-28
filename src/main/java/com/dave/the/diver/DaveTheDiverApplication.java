602b26bcfa5f7379cd08dfdf781d949bfb3f9755
Black-Garlic <hn1226@naver.com>
2024-02-28 23:17:28 +0900

    Init Project

---
+++ src/main/java/com/dave/the/diver/DaveTheDiverApplication.java
@@ -0,0 +1,13 @@
+package com.dave.the.diver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaveTheDiverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaveTheDiverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dave/the/diver/DaveTheDiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dave.the.diver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaveTheDiverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
