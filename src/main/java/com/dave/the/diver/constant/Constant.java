ca8c4000f51b70021aecf713f5adb73930f1a996
haneul <hn1226@naver.com>
2024-03-11 16:13:18 +0900

    Rank enum 추가

--- src/main/java/com/dave/the/diver/constant/Constant.java
+++ src/main/java/com/dave/the/diver/constant/Constant.java
@@ -14,4 +14,17 @@ public class Constant {
         public static int NINE = 9;
         public static int TEN = 10;
     }
+
+    public static class RANK {
+        public static int ONE = 1;
+        public static int TWO = 2;
+        public static int THREE = 3;
+        public static int FOUR = 4;
+        public static int FIVE = 5;
+        public static int SIX = 6;
+        public static int SEVEN = 7;
+        public static int EIGHT = 8;
+        public static int NINE = 9;
+        public static int MAX = 99;
+    }
 }
